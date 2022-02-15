27383a10457c7e0e82095968d2f95d7d413c0c86
isaksolheim <isak.m.h.s@gmail.com>
2022-02-15 21:38:51 +0100

    Add check OV functionality

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 const { App } = require('@slack/bolt');
+const axios = require('axios');
+require('dotenv').config();
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.APP_TOKEN,
 });
 
+const getOvStatus = async () => {
+  let status = false;
+
+  await axios
+    .get('https://omegav.no/')
+    .then((response) => {
+      status = response.data.includes('Omega Verksted er åpent!')
+        ? true
+        : false;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return status;
+};
+
 app.message(async ({ message, say }) => {
-  const reversedText = [...message.text].reverse().join('');
-  await say(reversedText);
+  if (message.text === 'OV?') {
+    const ovStatus = getOvStatus();
+
+    ovStatus ? await say('OV!') : await say(':disagreeing_astrid:');
+  }
 });
 
 (async () => {
-  // Start the app
   await app.start(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running!');
