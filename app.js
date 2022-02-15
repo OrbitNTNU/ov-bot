ab13330a355ca53bf929e6ce8e3edf556a3707f5
isaksolheim <isak.m.h.s@gmail.com>
2022-02-15 21:10:56 +0100

    Add boltjs boilerplate

---
+++ app.js
@@ -0,0 +1,18 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+app.message(async ({ message, say }) => {
+  const reversedText = [...message.text].reverse().join('');
+  await say(reversedText);
+});
+
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
