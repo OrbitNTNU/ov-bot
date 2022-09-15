95cc58bc1a9a5bc10464cd3958eb3d440ef04559
isaksolheim <mail@isaks.io>
2022-09-15 18:29:01 +0200

    Refactor comments

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const { App } = require('@slack/bolt');
 const axios = require('axios');
 require('dotenv').config();
 
-/* It creates a new instance of the App class, and sets the signingSecret, token, socketMode and
-appToken. */
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
@@ -11,7 +9,6 @@ const app = new App({
   appToken: process.env.APP_TOKEN,
 });
 
-/* Getting the environment variables from the .env file. */
 const omegaURL = process.env.OMEGA_URL;
 const apiURL = process.env.API_URL;
 const token = process.env.X_MASTER_KEY;
@@ -19,7 +16,6 @@ const chooChooURL = process.env.CHOO_CHOO_URL;
 const deployHookUrl = process.env.DEPLOY_HOOK_URL;
 const delpoyChannelId = process.env.DEPLOY_CHANNEL_ID;
 
-/* Setting the headers for the axios requests. */
 var axiosHeaders = {
   headers: {
     'Content-Type': 'application/json',
@@ -27,10 +23,6 @@ var axiosHeaders = {
   },
 };
 
-/**
- * It gets the current number of visits from the database, adds one to it, and then updates the
- * database with the new number of visits
- */
 const addVisit = async () => {
   const currentVisits = await getVisits();
 
@@ -45,11 +37,6 @@ const addVisit = async () => {
     .catch((error) => console.log(error));
 };
 
-/**
- * It fetches the HTML from the Omega website, and checks if the text "Omega Verksted er åpent!" is
- * present. If it is, it returns true, otherwise it returns false
- * @returns A function that returns a boolean value.
- */
 const getOvStatus = async () => {
   let status = false;
 
@@ -67,10 +54,6 @@ const getOvStatus = async () => {
   return status;
 };
 
-/**
- * It makes a GET request to the API endpoint, and returns the number of visits
- * @returns The number of visits to the website.
- */
 const getVisits = async () => {
   let visits = 0;
 
@@ -84,7 +67,6 @@ const getVisits = async () => {
   return visits;
 };
 
-/* This is a function that is called when a message is sent in a channel. */
 app.message(async ({ message, say }) => {
   if (message.text === 'OV?') {
     const ovStatus = await getOvStatus();
@@ -95,7 +77,6 @@ app.message(async ({ message, say }) => {
   }
 });
 
-/* This is a function that is called when a message is sent in a channel. */
 app.message(async ({ message, say }) => {
   if (message.text === 'OV#') {
     const visits = await getVisits();
@@ -104,7 +85,6 @@ app.message(async ({ message, say }) => {
   }
 });
 
-/* A command that is used to check the status of OV. */
 app.command('/ov-status', async ({ ack, respond }) => {
   await ack();
 
@@ -123,7 +103,6 @@ app.command('/ov-status', async ({ ack, respond }) => {
   );
 });
 
-/* A command that is used to start the OV-toget. */
 app.command('/start-train', async ({ ack, say }) => {
   await ack();
 
@@ -138,11 +117,9 @@ app.command('/start-train', async ({ ack, say }) => {
   );
 });
 
-/* A command that is used to deploy our website. */
 app.command('/deploy-website', async ({ ack, say, command }) => {
   await ack();
 
-  /* This is a check to see if the command is called from the correct channel. */
   if (command.channel_id === delpoyChannelId) {
     await axios.post(deployHookUrl).catch(async (error) => {
       console.log(error);
@@ -156,7 +133,6 @@ app.command('/deploy-website', async ({ ack, say, command }) => {
   }
 });
 
-/* A function that is called when the command `/help` is called. */
 app.command('/help', async ({ ack, say }) => {
   await ack();
 
@@ -170,7 +146,6 @@ app.command('/help', async ({ ack, say }) => {
     - /help - Shows available commands :muscle:`);
 });
 
-/* This is a function that is called when the app is started. */
 (async () => {
   await app.start(process.env.PORT || 3000);
 
